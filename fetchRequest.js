9a96be7919ac4db483117903831327226a3622aa
A K <129678194+asseavas@users.noreply.github.com>
2024-05-10 23:29:49 +0600

    Add files via upload

---
+++ fetchRequest.js
@@ -0,0 +1,9 @@
+const fetchRequest = async (url, init) => {
+    const response = await fetch(url, init);
+
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+
+    return response.json();
+};
